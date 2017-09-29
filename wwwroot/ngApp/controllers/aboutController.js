ff7dd7ec1a16cc6edfea7974bdac06e6d772f027
Maiphuong Pham <you@ git config --global user.email maipham@ucdavis.edu>
2017-09-29 11:32:17 -0700

    Bloop

---
+++ wwwroot/ngApp/controllers/aboutController.js
@@ -0,0 +1,13 @@
+﻿class AboutController {
+    constructor() {
+        this.message = 'Hello from the about page!';
+    }
+
+
+    // initMap() {
+    //   let map = new google.maps.Map(document.getElementById('map'), {
+    //     center: {lat: -34.397, lng: 150.644},
+    //     zoom: 8
+    //   });
+    // }
+}
