8948ca2cf47f321925e5084cb5491c8ee1ab2e79
Joy Kimaru <Joy Kimaru>
2017-09-29 11:50:57 -0700

    updates chat function

--- wwwroot/ngApp/controllers/homeController.js
+++ wwwroot/ngApp/controllers/homeController.js
@@ -1,11 +1,12 @@
 ﻿class HomeController {
-    constructor() {
+    constructor($scope) {
         this.messages=[];
         this.message="";
         this.connection = new signalR.HubConnection('/chat');
         this.connection.start().then(() => this.connection.invoke('send', 'Hello'));
         this.connection.on('send', data => {
                     this.messages.push(data);
+                    $scope.$apply();
                 });
         
        
