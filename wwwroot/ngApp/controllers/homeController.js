2d73b8295d5510acb90c0ae66992c9a5047fc4c9
Jason Tennant <xcodula@icloud.com>
2017-09-29 17:57:13 -0700

    fixed refresh issue, refactored ctor on homeController

--- wwwroot/ngApp/controllers/homeController.js
+++ wwwroot/ngApp/controllers/homeController.js
@@ -1,36 +1,67 @@
 ﻿class HomeController {
-    constructor() {
-        let origin={ lat: 42.3601, lng: -71.0589 };
-        window.initMap = function () {
-            var options = { //map options
-                zoom: 8,
-                center: origin
-            }
+    constructor($scope) {
+        this.start = "";
+        this.end = "";
+        this.markers = [];
 
-            //new map
-            var map = new google.maps.Map(document.getElementById('map'), options);
+        this.origin= { lat: 42.3601, lng: -71.0589 };
 
-            //Listen for click on map
-            google.maps.event.addListener(map, 'click',
-                function (event) {
-                    //adds marker
-                    addMarker({
-                        coord: event.latLng,
-                        content: "Travel here?",
-                    });
-                    if (document.getElementById('start').value == "") {
-                        document.getElementById('start').value = event.latLng
-                    };
-                    google.maps.event.addListener(map, 'click',
-                        function (event) {
-                            //adds marker
-                            document.getElementById('end').value = event.latLng;
-                        })
-                });
+        let mapElement = document.getElementById('map');
+        this.map = new google.maps.Map(mapElement, {
+            zoom: 8,
+            center: this.origin
+        });     
+        
+        google.maps.event.addListener(this.map, 'click', (event) => {
+            this.addMarker({
+                coord: event.latLng,
+                content: "Travel here?",
+            }, this.map);
+                           
+            if (this.start == "") {
+                this.start = event.latLng;
+                $scope.$apply();
+            };
 
+            google.maps.event.addListener(this.map, 'click', (event) => {
+                this.end = event.latLng;
+                $scope.$apply();                
+            });
+        }); 
 
-            //Array of Markers
-            var markers = [{
+        let markerDetailList = this.getMarkerDetailList();        
+        markerDetailList.forEach((markerDetail) => {
+            this.addMarker(markerDetail, this.map);
+        });  
+    }
+    
+    addMarker(markerDetail, map) {
+        var marker = new google.maps.Marker({
+            position: markerDetail.coord,
+            map: map,
+            // icon:props.iconImage
+        });
+
+        if (markerDetail.iconImage) {
+            marker.setIcon(markerDetail.iconImage);
+        }
+
+        if (markerDetail.content) {
+            var infoWindow = new google.maps.InfoWindow({
+                content: markerDetail.content,
+            });
+
+            marker.addListener('click', function () {
+                infoWindow.open(map, marker);
+            });
+        }
+
+        this.markers.push(marker);
+    }
+    
+    getMarkerDetailList() {
+        return [
+            {
                 coord: { lat: 42.4668, lng: -70.9495 },
                 iconImage: 'https://orig00.deviantart.net/dc58/f/2016/016/1/5/free_dragonite_icon_by_rrrai-d9o6j6v.png',
                 content: '<h1>Take me to England</h1>'
@@ -40,37 +71,7 @@
                 iconImage: 'https://orig00.deviantart.net/bd7f/f/2012/030/3/3/px__dustmite_dragon_by_niaro-d4o58x4.gif',
                 content: '<h1>Boston is ok i guess</h1>'
             }
-            ]
-
-            //Loop through markers
-
-            for (var index = 0; index < markers.length; index++) {
-                addMarker(markers[index]);
-
-            }
-            //Add Marker Function
-
-            function addMarker(props) {
-                var marker = new google.maps.Marker({
-                    position: props.coord,
-                    map: map,
-                    // icon:props.iconImage
-                });
-
-                if (props.iconImage) {
-                    marker.setIcon(props.iconImage);
-                }
-                if (props.content) {
-                    var infoWindow = new google.maps.InfoWindow({
-                        content: props.content,
-                    });
-
-                    marker.addListener('click', function () {
-                        infoWindow.open(map, marker);
-                    });
-                }
-            }
-        }
+        ];
     }
 }
 
