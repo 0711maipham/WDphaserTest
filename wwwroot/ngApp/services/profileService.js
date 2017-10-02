63937e76ce30e77b3c81c80b7e5d620bca4cb009
Joy Kimaru <Joy Kimaru>
2017-10-02 12:52:39 -0700

    dashboard part2

--- wwwroot/ngApp/services/profileService.js
+++ wwwroot/ngApp/services/profileService.js
@@ -2,10 +2,18 @@ class ProfileService {
     constructor ($resource) {
         this.dragonResource = $resource("/api/profile");
         this.friendResource = $resource("/api/friend");
+        this.user ={
+                DisplayName: "Meme",
+                JoinDate: "September 5th 2017",
+                HomeTown: "Phoenix, Arizona"
+            };
     }
 
     getProfile(id) {
         return this.profileResource.get({id:id});
+    }
+    getUser(){
+        return this.user;
     }
 
 }
