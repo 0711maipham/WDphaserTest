8d76744024f7ace067cda3b21222878233b97a8c
Maiphuong Pham <you@ git config --global user.email maipham@ucdavis.edu>
2017-10-02 18:01:09 -0700

    polished travelstart

--- wwwroot/ngApp/controllers/dynamicMapController.js
+++ wwwroot/ngApp/controllers/dynamicMapController.js
@@ -4,11 +4,11 @@
         this.end = "";
         this.markers = [];
 
-        this.origin= { lat: 42.3601, lng: -71.0589 };
+        this.origin= { lat: 42.3601, lng: -71.0589 }; //replace with this.origin = dragon.hometown
 
         let mapElement = document.getElementById('map');
         this.map = new google.maps.Map(mapElement, {
-            zoom: 8,
+            zoom: 6,
             center: this.origin
         });     
         
@@ -18,15 +18,11 @@
                 content: "Travel here?",
             }, this.map);
                            
-            if (this.start == "") {
-                this.start = event.latLng;
+            if (this.end == "") {
+                this.end = event.latLng.toString().replace(/[{()}]/g, '');
                 $scope.$apply();
             };
 
-            google.maps.event.addListener(this.map, 'click', (event) => {
-                this.end = event.latLng;
-                $scope.$apply();                
-            });
         }); 
 
         let markerDetailList = this.getMarkerDetailList();        
@@ -34,6 +30,22 @@
             this.addMarker(markerDetail, this.map);
         });  
     }
+
+    travelCheck(destination) {
+        var x = destination.split(',').map(Number);
+        var lat1 = this.origin.lat;
+        var lon1 = this.origin.lng;
+        var lat2 = x[0];
+        var lon2 = x[1];
+        var p = 0.017453292519943295;    // Math.PI / 180
+        var c = Math.cos;
+        var a = 0.5 - c((lat2 - lat1) * p)/2 + 
+                c(lat1 * p) * c(lat2 * p) * 
+                (1 - c((lon2 - lon1) * p))/2;
+      
+        var y = 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+        return y;
+      }
     
     addMarker(markerDetail, map) {
         var marker = new google.maps.Marker({
@@ -62,88 +74,13 @@
     getMarkerDetailList() {
         return [
             {
-                coord: { lat: 42.4668, lng: -70.9495 },
-                iconImage: 'https://orig00.deviantart.net/dc58/f/2016/016/1/5/free_dragonite_icon_by_rrrai-d9o6j6v.png',
+                coord: this.origin,
+                iconImage: 'https://orig00.deviantart.net/dc58/f/2016/016/1/5/free_dragonite_icon_by_rrrai-d9o6j6v.png', //replace with dragon.img
                 content: '<h1>Take me to England</h1>'
             },
-            {
-                coord: { lat: 42.8584, lng: -70.9300 },
-                iconImage: 'https://orig00.deviantart.net/bd7f/f/2012/030/3/3/px__dustmite_dragon_by_niaro-d4o58x4.gif',
-                content: '<h1>Boston is ok i guess</h1>'
-            }
         ];
     }
 }
 
-    // initMap() {
-    //     var options = { //map options
-    //         zoom: 8,
-    //         center: { lat: 42.3601, lng: -71.0589 }
-    //     }
-
-    //     //new map
-    //     var map = new google.maps.Map(document.getElementById('map'), options);
-
-    //     //Listen for click on map
-    //     google.maps.event.addListener(map, 'click',
-    //         function (event) {
-    //             //adds marker
-    //             addMarker({
-    //                 coord: event.latLng,
-    //                 content: "Travel here?",
-    //             });
-    //             if (document.getElementById('start').value == "") {
-    //                 document.getElementById('start').value = event.latLng
-    //             };
-    //             google.maps.event.addListener(map, 'click',
-    //                 function (event) {
-    //                     //adds marker
-    //                     document.getElementById('end').value = event.latLng;
-    //                 })
-    //         });
-
-
-    //     //Array of Markers
-    //     var markers = [{
-    //         coord: { lat: 42.4668, lng: -70.9495 },
-    //         iconImage: 'https://orig00.deviantart.net/dc58/f/2016/016/1/5/free_dragonite_icon_by_rrrai-d9o6j6v.png',
-    //         content: '<h1>Take me to England</h1>'
-    //     },
-    //     {
-    //         coord: { lat: 42.8584, lng: -70.9300 },
-    //         iconImage: 'https://orig00.deviantart.net/bd7f/f/2012/030/3/3/px__dustmite_dragon_by_niaro-d4o58x4.gif',
-    //         content: '<h1>Boston is ok i guess</h1>'
-    //     }
-    //     ]
-
-    //     //Loop through markers
-
-    //     for (var index = 0; index < markers.length; index++) {
-    //         addMarker(markers[index]);
-
-    //     }
-    //     //Add Marker Function
-
-    //     function addMarker(props) {
-    //         var marker = new google.maps.Marker({
-    //             position: props.coord,
-    //             map: map,
-    //             // icon:props.iconImage
-    //         });
-
-    //         if (props.iconImage) {
-    //             marker.setIcon(props.iconImage);
-    //         }
-    //         if (props.content) {
-    //             var infoWindow = new google.maps.InfoWindow({
-    //                 content: props.content,
-    //             });
-
-    //             marker.addListener('click', function () {
-    //                 infoWindow.open(map, marker);
-    //             });
-    //         }
-    //     }
-    // }
 
 
